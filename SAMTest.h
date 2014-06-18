54a870d7c660726fcc33f06e49edf3b0695603ce
Shafiq Marediya <shafiq702@Shafiq-Marediyas-MacBook-Pro.local>
2014-06-17 21:47:55 -0500

    added a test class

---
+++ SAMTest.h
@@ -0,0 +1,13 @@
+//
+//  SAMTest.h
+//  Git Test
+//
+//  Created by Shafiq Marediya on 6/17/14.
+//  Copyright (c) 2014 Shafiq Marediya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAMTest : NSObject
+
+@end
